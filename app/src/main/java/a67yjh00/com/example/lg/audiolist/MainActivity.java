70394cb9569eed755955f5a7f19df77e70a08948
yoonjoohye <67yjh00@gmail.com>
2017-06-19 20:58:27 +0900

    #4

--- app/src/main/java/a67yjh00/com/example/lg/audiolist/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/audiolist/MainActivity.java
@@ -1,13 +1,66 @@
 package a67yjh00.com.example.lg.audiolist;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView list;
+    Button butPlay, butStop;
+    TextView textMusic;
+    ProgressBar progress;
+    String [] a={"dinos","fire","ncdinos"};
+    int [] musicResIds={R.raw.dinos,R.raw.fire,R.raw.ncdinos};
+    int selectedMusicId;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.list_music);
+        butPlay=(Button)findViewById(R.id.but_play);
+        butStop=(Button)findViewById(R.id.but_stop);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar) findViewById(R.id.progress_music);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,a);
+        list.setAdapter(adapter);
+        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);//여러항목중 하나만 선택가능
+        list.setItemChecked(0,true);//실행
+        selectedMusicId=musicResIds[0];
+        mediaPlayer=MediaPlayer.create(this,selectedMusicId);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                selectedMusicId=musicResIds[i];
+
+            }
+        });//항목이 클릭되었을 때 처리
+        butPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        butStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.create(MainActivity.this,selectedMusicId);
+                mediaPlayer.stop();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mediaPlayer.stop();
     }
 }
